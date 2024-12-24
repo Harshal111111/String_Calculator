6a7e3f21b30b4bc8b4319fdbbb090f072d345efc
Harshal <bordiaharshal@gmail.com>
2024-12-24 14:43:18 +0530

    refactored-code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,20 +1,25 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.length == 1
-    numbers.split(",").map(&:to_i).sum
-    numbers.gsub("\n",",").split(",").map(&:to_i).sum
-
+    delimiter, numbers = extract_delimiter(numbers)
+    sum(numbers, delimiter)
+  end
+  private
+  def extract_delimiter(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers = numbers.split("\n", 2)[1]
     else
+      delimiter = /[^0-9\-]+/
       numbers = numbers.gsub("\n",",")
     end
-    array_of_numbers = numbers.split(/[^0-9\-]+/).map(&:to_i)
+    [delimiter, numbers]
+  end
+
+  def sum(numbers, delimiter)
+    array_of_numbers = numbers.split(delimiter).map(&:to_i)
     negatives = array_of_numbers.select { |n|  n < 0 }
     raise "negative numbers not allowed #{negatives.join(",")}" if negatives.any? 
     array_of_numbers.sum
   end
-  
 end

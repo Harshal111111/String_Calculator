285ffa561680655df34ba5b11fc3085b64727b78
Harshal <bordiaharshal@gmail.com>
2024-12-24 11:49:15 +0530

    fixed-failing-test-for-empty-string

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -3,7 +3,7 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
   describe 'addition' do
     it "returns 0 for an empty string" do 
-      expect(StringCalculator.new.add("").to eq(0))
+      expect(StringCalculator.new.add("")).to eq(0)
     end
   end
 end

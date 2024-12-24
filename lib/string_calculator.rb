27f4141a3fb79878ebfa04551b6fa6a810f8c730
Harshal <bordiaharshal@gmail.com>
2024-12-24 12:19:26 +0530

    fixed-test-for-new-line-between-numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,7 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
     numbers.split(",").map(&:to_i).sum
+    numbers.gsub("\n",",").split(",").map(&:to_i).sum
   end
   
 end
